c599c05527b9b45ee0220810b7b48cff5ba709dc
Atif <atif.anwer@insa-rouen.fr>
2024-01-29 12:34:25 +0100

    generating keypoints working. Need to confirm if correct

--- src/video_JSON.py
+++ src/video_JSON.py
@@ -91,31 +91,37 @@ def vid_to_JSON() -> None:
 				# qq = (coords_dict['x'], coords_dict['y'], coords_dict['visibility'])
 				# tmp.append(qq)
 
-			#  SCALING the x and y coordinates with the resolution of the image to get px corrdinates
-			for i in range(len(pose)):
-				pose[i] = ( round( (np.multiply(pose[i][0], width)), 3), \
-						round( (np.multiply(pose[i][1], height)) , 3) )
 			# Calculate the two additional joints for openpose and add them
 			# NECK KPT
-			pose[1] = ( 	(pose[11][0] - pose[12][0]) / 2 + pose[12][0], \
+			neck_kpt = ( 	(pose[11][0] - pose[12][0]) / 2 + pose[12][0], \
 					(pose[11][1] + pose[12][1]) / 2 )
-			# saving the hip mid point in the list for later use
-			stash = pose[8]
-			# HIP_MID
-			pose.append(stash)
-			pose[8] = ( (pose[23][0] - pose[24][0]) / 2 + pose[24][0], \
-					(pose[23][1] + pose[24][1]) / 2 )
+			# replacing pose[1] (left-eye-inner) with the neck kpt
+			pose[1] = neck_kpt
 
+			#  SCALING the x and y coordinates with the resolution of the image to get px corrdinates
+			for i in range(len(pose)):
+				pose[i] = ( 	round( (np.multiply(pose[i][0], width)), 3), \
+						round( (np.multiply(pose[i][1], height)), 3) )
+
+			# saving the hip mid point in the list for later use
+			# stash = pose[8]
+			# HIP_MID
+			# pose.append(stash)
+			# mid_hip_kpt = ( (pose[23][0] - pose[24][0]) / 2 + pose[24][0], \
+			# 		(pose[23][1] + pose[24][1]) / 2 )
+			# pose[8] = mid_hip_kpt
 			# Reordering list to comply to openpose format
 			# For the order table,refer to the Notion page
-			# restoring the saved hip mid point
-			mp_to_op_reorder = [0, 1, 12, 14, 16, 11, 13, 15, 8, 24, 26, 28, 23, 25, 27, 5, 2, 33, 7, 31, 31, 29, 32, 32, 30, 0, 0, 0, 0, 0, 0, 0, 0]
+			# body25__reorder = [0, 1, 12, 14, 16, 11, 13, 15, 8, 24, 26, 28, 23, 25, 27, 5, 2, 33, 7, 31, 31, 29, 32, 32, 30, 0, 0, 0, 0, 0, 0, 0, 0]
+			coco17_reorder = [0, 1, 12, 14, 16, 11, 13, 15, 24, 26, 28, 23, 25, 27, 5, 2, 8, 7]
 
-			onlyList = [pose[i] for i in mp_to_op_reorder]
+			onlyList = [pose[i] for i in coco17_reorder]
+			score = [score[i] for i in coco17_reorder]
+
 			score    = [[i] for i in score]
 			# delete the last 8 elements to conform to OpenPose joint length of 25
-			del onlyList[-8:]
-			del score[-8:]
+			# del onlyList[-8:]
+			# del score[-8:]
 
 			# # Converting form Openpose Body25 to COCO 17
 			# # coco17_order = [0, 1, 2, 3, 4, 5, 6, 7, 0, 8, 9, 10, 11, 12, 13, 0, 14, 15, 16, 17, 0, 0, 0, 0, 0]
